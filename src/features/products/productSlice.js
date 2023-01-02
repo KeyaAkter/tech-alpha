aa72c04f741d8980e49f123efada145625a64775
KeyaAkter <sanhatanvin12@gmail.com>
2023-01-02 20:55:10 +0600

    redux folder  setup completed

--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -1,1 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+import axios from "axios";
+
+const initialState = {
+  items: [],
+  status: null,
+};
+
+export const productsFetching = createAsyncThunk(
+  "products/productsFetching",
+  async () => {
+    const res = await axios.get(
+      "https://eager-sable-airedale.glitch.me/products"
+    );
+
+    return res.data;
+  }
+);
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(productsFetching.pending, (state, action) => {
+      state.status = "pending";
+    });
+
+    builder.addCase(productsFetching.fulfilled, (state, action) => {
+      state.status = "successfull";
+      state.items = action.payload;
+    });
+
+    builder.addCase(productsFetching.rejected, (state, action) => {
+      state.status = "rejected";
+    });
+  },
+});
+
+export default productsSlice.reducer;
