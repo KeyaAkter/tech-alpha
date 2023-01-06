81e1d3ddbddd32f63faaa46a416bc0703d2dbc86
KeyaAkter <sanhatanvin12@gmail.com>
2023-01-06 17:28:30 +0600

    not found page added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found container mx-auto mt-20 text-center">
+      <h2 className="text-4xl font-bold text-rose-500">404</h2>
+      <p className="text-2xl mb-10">Page not found!</p>
+      <Link to="/">
+        <button className="bg-violet-700 text-violet-50 px-6 py-3 uppercase tracking-widest font-medium  hover:bg-orange-500 hover:text-orange-50 duration-300">
+          Go to home
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
